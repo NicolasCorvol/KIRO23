60bac47918e18fd2587c3846a780626b002e0d25
NicolasCorvol <nicolas.corvol@gmail.com>
2023-11-09 09:36:17 -0500

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+import json 
+import numpy as np 
+
+from classes import *
+
+instances_size = ['toy', 'small', 'medium', 'large', 'huge']
+
+size = instances_size[0]
+
+pathfile = f'./instances/{size}.json'
+
+
