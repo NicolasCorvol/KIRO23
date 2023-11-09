9c97af9a95674bb64fe1e509f9d61e648fcb7de1
NicolasCorvol <nicolas.corvol@gmail.com>
2023-11-09 11:00:56 -0500

    rien

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ instances_size = ['toy', 'small', 'medium', 'large', 'huge']
 size = instances_size[0]
 
 pathfile = f'./instances/{size}.json'
+
+f = open(pathfile)
+ 
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+
+len(data['substation_location'])
